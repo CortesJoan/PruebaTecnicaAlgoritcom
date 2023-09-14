8cbfc3108901cc05ac80c3bb5d89530d5f5ac003
Joan <cortesjoancontact@gmail.com>
2023-09-14 19:56:18 +0200

    Improved PlayerMovement
    Now is relative to the current camera

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -3,53 +3,115 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidbody;
+    [Header("References")]
+    [SerializeField] private Rigidbody playerRigidbody;
 
+    [Header("Config")] 
+    [SerializeField] private float moveSpeed;
+    public Transform moveRelativeTo;
+
+    [Header("Debug")] 
     [SerializeField] private Vector3 currentMoveDirection;
-    [SerializeField] private float moveForce;
+    private bool canMove = true;
+    private Vector3 previousVelocity;
+    private Vector3 currentVelocity;
+    private const float minimumVelocityMagnitudeToStop = 0.1f;
 
+    [Header("Events")]
+    [SerializeField] private UnityEvent onMoveStopped;
+    [SerializeField] private UnityEvent onMoveStarted;
 
-    [SerializeField] private Vector3 currentVelocity;
 
     public void ChangeMoveDirection(Vector3 newMoveDirection)
     {
         currentMoveDirection = newMoveDirection;
     }
 
+    public void ChangeMoveSpeed(float newSpeed)
+    {
+        this.moveSpeed = newSpeed;
+    }
+
+    private void Update()
+    {
+        if (currentMoveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(currentMoveDirection);
+        }
+    }
 
     public void FixedUpdate()
     {
-        rigidbody.velocity = currentMoveDirection * (moveForce * Time.fixedDeltaTime);
-        //      rigidbody.velocity = transform.TransformDirection(currentMoveDirection) * (moveForce * Time.fixedDeltaTime);
-        currentVelocity = rigidbody.velocity;
+        previousVelocity = playerRigidbody.velocity;
+
+        currentVelocity = currentMoveDirection * (moveSpeed * Time.fixedDeltaTime);
+        playerRigidbody.velocity = currentVelocity;
+        FireMoveEvents();
     }
 
+    void FireMoveEvents()
+    {
+        if (previousVelocity.magnitude > minimumVelocityMagnitudeToStop &&
+            currentVelocity.magnitude < minimumVelocityMagnitudeToStop)
+        {
+            onMoveStopped?.Invoke();
+        }
+        else if (previousVelocity.magnitude < minimumVelocityMagnitudeToStop &&
+                 currentVelocity.magnitude > minimumVelocityMagnitudeToStop)
+        {
+            onMoveStarted?.Invoke();
+        }
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (!canMove)
+        {
+            return;
+        }
         Vector2 moveDirection = context.ReadValue<Vector2>();
         // move direction possible values 
         // 1,0    X right  y rotation of 90
         //-1,0    X left y rotation of -90
         //0,1    Z forward y rotation of -180 
         //0,-1    Z backward  y rotation of 180
-
-
-        currentMoveDirection = new Vector3(moveDirection.x, currentMoveDirection.y, moveDirection.y);
+        if (moveRelativeTo == null)
+        {
+            moveRelativeTo = this.transform;
+        }
+        currentMoveDirection =
+            moveRelativeTo.TransformDirection(new Vector3(moveDirection.x, 0, moveDirection.y));
+        currentMoveDirection.y = 0;
         if (moveDirection.normalized == new Vector2(0.71f, 0.71f).normalized)
         {
             Debug.Log("true");
         }
         Debug.Log("Move direction " + moveDirection);
-        if (moveDirection != Vector2.zero)
-        {
-            // transform.localRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, transform.localRotation.y, moveDirection.y).normalized);
-            float angle = Mathf.Atan2(moveDirection.normalized.x, moveDirection.normalized.y);
-            angle *= Mathf.Rad2Deg;
-            transform.localEulerAngles = new Vector3(0, angle, 0);  
-        }
+    }
+
+    public void BlockMovement()
+    {
+        currentMoveDirection = Vector3.zero;
+        canMove = false;
+    }
+
+    public void UnblockMovement()
+    {
+        canMove = true;
+    }
+
+    public Transform GetMoveRelativeTo()
+    {
+        return moveRelativeTo;
+    }
+    
+    public void SetMoveRelativeTo(Transform newMoveRelativeTo)
+    {
+        moveRelativeTo = newMoveRelativeTo;
     }
 }
